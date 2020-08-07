0901030478f2502cdff2a9aad26d9f12860e6114
David Heath <david@davidheath.org>
2020-08-07 17:02:29 +0100

    Add rake task to expunge data more than 30 days old

--- lib/tasks/scheduler.rake
+++ lib/tasks/scheduler.rake
@@ -1,5 +1,11 @@
 desc "This task is called by the Heroku scheduler add-on"
 task :expunge_data => :environment do
-  puts "Expunging old data..."
-  puts "done."
+  threshold = 30.days.ago
+  Rails.logger.info "[expunge_data] Expunging data created on or before #{threshold}..."
+
+  [Vote, ProposedSolution, Participant, Challenge].each do |klass|
+    n = klass.where("created_at <= ?", threshold).delete_all
+    Rails.logger.info "[expunge_data] Deleted #{n} #{klass} records"
+  end
+  Rails.logger.info "[expunge_data] done."
 end
---
+++ spec/test/integration/expunge_data_rake_task_spec.rb
@@ -0,0 +1,52 @@
+require 'rake'
+require 'rails_helper'
+
+RSpec.describe "rake expunge_data" do
+
+  before do
+    Twentyfiveten::Application.load_tasks
+    Rake::Task.define_task(:environment)
+  end
+
+  def run_rake_task(task_name)
+    Rake::Task[task_name].reenable
+    Rake.application.invoke_task task_name
+  end
+
+  context "a completed challenge" do
+
+    before do
+      DatabaseCleaner.clean
+      Timecop.freeze(Time.local(2020, 1, 1, 12, 0, 0))
+      c = Challenge.create!(challenge_prompt: "Foo", administrator: Administrator.new)
+      p = Participant.create!(challenge_id: c.id)
+      p.create_proposed_solution!(challenge_id: c.id, narrative: "a", first_step: "b")
+      c.schedule_votes!
+    end
+
+    after { Timecop.return }
+
+    it "should not delete within 30 days" do
+      Timecop.freeze(29.days.from_now) do
+        run_rake_task("expunge_data")
+
+        expect(Challenge.count).to eq(1)
+        expect(Participant.count).to eq(1)
+        expect(ProposedSolution.count).to eq(1)
+        expect(Vote.count).to eq(5)
+      end
+    end
+
+    it "should delete 30 or older" do
+      Timecop.freeze(31.days.from_now) do
+        run_rake_task("expunge_data")
+
+        expect(Challenge.count).to eq(0)
+        expect(Participant.count).to eq(0)
+        expect(ProposedSolution.count).to eq(0)
+        expect(Vote.count).to eq(0)
+      end
+    end
+  end
+
+end
