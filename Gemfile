b625d9f3b6eef152d7ac1ee69facb8c1f44234d0
David Heath <david.heath@digital.cabinet-office.gov.uk>
2023-03-07 21:02:51 +0000

    Upgrade to rails 7
    remove webpacker

--- Gemfile
+++ Gemfile
@@ -4,19 +4,30 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby File.read(File.dirname(__FILE__) + '/.ruby-version')
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 6.0.3', '>= 6.0.3.5'
+gem 'rails'
+# The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
+gem "sprockets-rails"
 # Use postgresql as the database for Active Record
-gem 'pg', '>= 0.18', '< 2.0'
+gem 'pg', '>= 0.18'
 # Use Puma as the app server
-gem 'puma', '~> 4.3'
+gem 'puma'
+# Use JavaScript with ESM import maps [https://github.com/rails/importmap-rails]
+gem "importmap-rails"
+# Hotwire's SPA-like page accelerator [https://turbo.hotwired.dev]
+# gem "turbo-rails"
+# Hotwire's modest JavaScript framework [https://stimulus.hotwired.dev]
+# gem "stimulus-rails"
+
+# Use Tailwind CSS [https://github.com/rails/tailwindcss-rails]
+# gem "tailwindcss-rails"
 # Use SCSS for stylesheets
-gem 'sass-rails', '>= 6'
+gem 'sass-rails'
 # Transpile app-like JavaScript. Read more: https://github.com/rails/webpacker
-gem 'webpacker', '~> 4.0'
+# gem 'webpacker'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
-gem 'turbolinks', '~> 5'
+# gem 'turbolinks', '~> 5'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
-gem 'jbuilder', '~> 2.7'
+# gem 'jbuilder', '~> 2.7'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
@@ -28,22 +39,23 @@ gem 'jbuilder', '~> 2.7'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
 
+
 # CSS skeleton
 gem 'skeleton-rails', :git => 'https://github.com/helios-technologies/skeleton-rails'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
-  gem 'rspec-rails', '~> 4.0.1'
+  gem 'rspec-rails'
 end
 
 group :development do
   # Access an interactive console on exception pages or by calling 'console' anywhere in the code.
   gem 'web-console', '>= 3.3.0'
-  gem 'listen', '~> 3.2'
+  gem 'listen'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
-  gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'spring-watcher-listen'
 end
 
 group :test do
--- db/schema.rb
+++ db/schema.rb
@@ -2,23 +2,22 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_07_165351) do
-
+ActiveRecord::Schema[7.0].define(version: 2023_03_07_195547) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "administrators", force: :cascade do |t|
     t.string "administrator_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["administrator_id"], name: "index_administrators_on_administrator_id"
   end
 
@@ -29,8 +28,8 @@ ActiveRecord::Schema.define(version: 2020_08_07_165351) do
     t.string "challenge_context"
     t.string "status"
     t.integer "voting_round"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "challenge_prompt"
     t.string "first_step_prompt"
     t.index ["administrator_id"], name: "index_challenges_on_administrator_id"
@@ -38,8 +37,8 @@ ActiveRecord::Schema.define(version: 2020_08_07_165351) do
 
   create_table "participants", force: :cascade do |t|
     t.integer "challenge_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.boolean "consent"
     t.index ["challenge_id"], name: "index_participants_on_challenge_id"
   end
@@ -49,8 +48,8 @@ ActiveRecord::Schema.define(version: 2020_08_07_165351) do
     t.integer "participant_id"
     t.string "narrative"
     t.string "first_step"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["challenge_id"], name: "index_proposed_solutions_on_challenge_id"
     t.index ["participant_id"], name: "index_proposed_solutions_on_participant_id"
   end
@@ -61,8 +60,8 @@ ActiveRecord::Schema.define(version: 2020_08_07_165351) do
     t.integer "participant_id"
     t.integer "vote"
     t.integer "round"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["challenge_id"], name: "index_votes_on_challenge_id"
     t.index ["participant_id"], name: "index_votes_on_participant_id"
     t.index ["proposed_solution_id"], name: "index_votes_on_proposed_solution_id"
